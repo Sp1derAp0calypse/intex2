64b1468f22df00e90a5440d7dae32c4e3a5d70e2
Sp1derAp0calypse <131923585+Sp1derAp0calypse@users.noreply.github.com>
2025-04-07 17:55:13 -0600

    Add files via upload

---
+++ backend/Intex.API/Data/ContentRecommend.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Intex.API.Models
+{
+    public class ContentRecommend
+    {
+        public int Index { get; set; }
+        public string IfYouLiked { get; set; }
+        public string Recommendation1 { get; set; }
+        public string Recommendation2 { get; set; }
+        public string Recommendation3 { get; set; }
+        public string Recommendation4 { get; set; }
+        public string Recommendation5 { get; set; }
+    }
+}
