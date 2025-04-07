498f7c541fb56e54091718782dff530698f9774c
alextperry <alextperry@outlook.com>
2025-04-07 11:22:38 -0600

    initial changes

---
+++ backend/Intex.API/Controllers/MovieController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Intex.API.Controllers
+{
+
+    [Route("[controller]")]
+    [ApiController]
+    [Authorize]
+
+    public class MovieController : ControllerBase
+    {
+
+
+
+    }
+}
--- backend/Intex.API/Program.cs
+++ backend/Intex.API/Program.cs
@@ -7,6 +7,12 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// input the db connection strings here: 
+
+// using authorization 
+builder.Services.AddAuthorization();
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
