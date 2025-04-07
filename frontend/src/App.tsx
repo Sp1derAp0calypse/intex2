ff878b6b7c65dbc099c4bae69800c0b8a7feecf3
alextperry <alextperry@outlook.com>
2025-04-07 14:39:40 -0600

    imported the movies.db

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,20 @@
 import "./App.css";
+import LoginPage from "./LoginPage";
 import MovieList from "./MovieList";
 import NavBar from "./NavBar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <NavBar />
-      <MovieList />
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<MovieList />} />
+          <Route path="/movielist" element={<MovieList />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </Router>
     </>
   );
 }
