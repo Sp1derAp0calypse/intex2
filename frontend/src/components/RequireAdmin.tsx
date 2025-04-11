1857439bf2bd1d4f826fd03b694a46ea8dfac5c6
Sp1derAp0calypse <131923585+Sp1derAp0calypse@users.noreply.github.com>
2025-04-11 00:05:04 -0600

    Update RequireAdmin.tsx

--- frontend/src/components/RequireAdmin.tsx
+++ frontend/src/components/RequireAdmin.tsx
@@ -1,8 +1,6 @@
 import { JSX, useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 
 const RequireAdmin = ({ children }: { children: JSX.Element }) => {
-  const navigate = useNavigate();
   const [checking, setChecking] = useState(true);
   const [authorized, setAuthorized] = useState(false);
 
@@ -10,8 +8,8 @@ const RequireAdmin = ({ children }: { children: JSX.Element }) => {
     const checkAdmin = async () => {
       try {
         const response = await fetch(
-          // "https://intex2-315-backend-gxdsgxfwavhyc8ax.eastus-01.azurewebsites.net/pingauth",
           "https://localhost:5000/pingauth",
+          // "https://intex2-315-backend-gxdsgxfwavhyc8ax.eastus-01.azurewebsites.net/pingauth",
           {
             credentials: "include",
           }
@@ -22,21 +20,19 @@ const RequireAdmin = ({ children }: { children: JSX.Element }) => {
         const data = await response.json();
         if (data.roles && data.roles.includes("Administrator")) {
           setAuthorized(true);
-        } else {
-          navigate("/userHomePage"); // or a 403 page
         }
       } catch (err) {
         console.error("Access denied:", err);
-        navigate("/login");
       } finally {
         setChecking(false);
       }
     };
 
     checkAdmin();
-  }, [navigate]);
+  }, []);
 
-  if (checking) return <p>Checking access...</p>;
+  if (checking) return null;
+
   return authorized ? children : null;
 };
 
